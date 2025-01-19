7c162e073b10ce05974c18760e7e232a90841411
Febin Johnson <febinnj1996@gmail.com>
2025-01-19 12:31:04 +0530

    added approutes

---
+++ lib/presentation/home/home_ui.dart
@@ -0,0 +1,11 @@
+import 'package:edstem_machinetest/presentation/home/ui/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatelessWidget {
+  const HomeUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
